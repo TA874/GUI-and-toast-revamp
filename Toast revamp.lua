b22077ad6be3c8a6fbb7dadcc6e381b638a32dc7
jeremiahstephens464 <78432747+jeremiahstephens464@users.noreply.github.com>
2022-03-26 19:57:17 +0200

    Update Toast revamp.lua

--- Toast revamp.lua
+++ Toast revamp.lua
@@ -11,7 +11,7 @@ local function drawOutline(x,y,w,h,s)
 	Drawing.drawRect(x+w-s,y,s,s)
 	Drawing.drawRect(x+w-s,y+h-s,s,s)
 end
-local giAutoGetColor,giGetColor=function()
+local giAutoGetColor,giGetColor,isToastRevampEnabled=function()
 	local br,bg,bb=0,0,0
 	if type(giAutoGetColor)=="function" then br,bg,bb=giAutoGetColor() end
 	return br,bg,bb
@@ -20,7 +20,8 @@ function()
 	local cr,cg,cb=255,255,255
 	if type(giGetColor)=="function" then cr,cg,cb=giGetColor() end
 	return cr,cg,cb
-end
+end,
+function() if type(isToastRevampEnabled)=="function" then return isToastRevampEnabled() end end
 if type(toasts)~="table" then toasts={} end
 if type(setClipboard2)~="function" then setClipboard2=Runtime.setClipboard end
 function table.copy(tbl) if type(tbl)=="table" then
@@ -43,72 +44,136 @@ local function findToast(id)
 	end
 	return tbl,i
 end
-local addToast2=function(tbl)
+local addToast2 addToast2=function(tbl)
 	local c=GUI.get("toasts")
 	if type(c)=="table" then
-		c:setChildIndex(c:getParent():countChildren()+1)
-		return c:addCanvas {
+		local gth,thh=12,0
+		local ww=function() return 0 end
+		local dismissButton
+		local cc=c:addCanvas {
 			h=0,
 			onInit=function(self)
 				self:setX(self:getParent():getWidth())
 				self:setTouchThrough(true)
+				local c=self:getParent()
+				c:setChildIndex(c:getParent():countChildren()+1)
+				dismissButton=self:addCanvas {
+					w=0,
+					onInit=function(self)
+						local s=0.7
+						local text="Dismiss"
+						local tw,th=Drawing.getTextSize(text,Font.BIG)
+						self:setSize(tw*s+5,15)
+						ww=function() return self:getWidth() end
+					end,
+					onUpdate=function(self)
+						self:setPo(self:getParent():getW()-self:getW(),0)
+					end,
+					onDraw=function(self,x,y,w,h)
+						local ttt=(Runtime.getTime()-tbl.tt)/1000
+						local time=(tonumber(tbl.time) or 1000)/1000
+						local startTime=(tonumber(tbl.startTime) or 1000)/1000
+						local endTime=(tonumber(tbl.endTime) or 1000)/1000
+						if not TheoTown.SETTINGS.uiAnimations then startTime,endTime=0,0 end
+						self:setTouchThrough(ttt<startTime and ttt>=startTime+time)
+						local s=0.7
+						local text="Dismiss"
+						local tw,th=Drawing.getTextSize(text,Font.BIG)
+						self:setSize(tw*s+5,15)
+						w,h=self:getSize()
+						Drawing.setColor(giAutoGetColor())
+						if (ttt>=startTime and ttt<startTime+time) and (self:getTouchPoint() or self:isMouseOver()) then
+							Drawing.setAlpha(0.2)
+							if self:getTouchPoint() then Drawing.setAlpha(0.3) end
+							Drawing.drawRect(x,y+1,w-1,h-1)
+							Drawing.setAlpha(1)
+						end
+						Drawing.setAlpha(0.5)
+						Drawing.setScale(s,s)
+						if ttt>=startTime and ttt<startTime+time then Drawing.drawText(text,x+(w*0.5)-((tw*s)*0.5),y+(h*0.5)-((th*s)*0.5),Font.BIG) end
+						Drawing.setScale(1,1)
+						Drawing.setAlpha(1)
+						Drawing.setColor(255,255,255)
+					end,
+					onClick=function()
+						local ttt=Runtime.getTime()-tbl.tt
+						local startTime=tonumber(tbl.startTime) or 1000
+						local endTime=tonumber(tbl.endTime) or 1000
+						if not TheoTown.SETTINGS.uiAnimations then startTime,endTime=0,0 end
+						if ttt>=startTime and ttt<startTime+tbl.time then tbl.tt=Runtime.getTime()-(startTime+tbl.time) end
+					end
+				}
 			end,
 			onUpdate=function(self)
 				local tbl=findToast(tbl.id)
 				if not tbl then self:delete() end
 			end,
 			onDraw=function(self,x,y,w,h)
+				self:setWidth(0)
 				local s=0.7
 				local y0=0
 				local ttt=(Runtime.getTime()-tbl.tt)/1000
-				--local uis=TheoTown.SETTINGS.uiSize+0
-				--local x,y=(GUI.getRoot():getWidth()-p2-p4)+(50*(((0+uis)/2)*1.6)),p3-1
-				local xx=x
-				pcall(function() xx=self:getParent():getWidth() end)
-				xx=tonumber(xx) or x
-				local text=tostring(tbl.text or "")
-				local tw,th=Drawing.getTextSize(text,Font.BIG)
-				local title=tostring(tbl.title or "")
-				local tw2=Drawing.getTextSize(title,Font.BIG)
-				tw,th,tw2=tw*s,th*s,tw2*s
-				Drawing.setColor(giGetColor())
-				local hh=30
+				local startTime=(tonumber(tbl.startTime) or 1000)/1000
+				local endTime=(tonumber(tbl.endTime) or 1000)/1000
+				if not TheoTown.SETTINGS.uiAnimations then startTime,endTime=0,0 end
+				local time=(tonumber(tbl.time) or 1000)/1000
+				local hh=15+thh
 				local ii,iii=1,1
-				if ttt>=4.7 then iii=1-((ttt-4.7)/0.3)
-				else ii=math.min(1,ttt/0.3) end
-				do local ii=ii if ttt>0.3 then ii=1 end hh=(30*ii) end
+				if ttt>=startTime+time then iii=1-((ttt-(startTime+time))/startTime) else ii=math.min(1,ttt/startTime) end
+				if ttt>=startTime+endTime+time then ii=1-math.min(1,(ttt-(startTime+endTime+time))/endTime) end
+				if not TheoTown.SETTINGS.uiAnimations then ii,iii=1,1 end
+				do local ii=ii if ttt>=startTime and ttt<(startTime+endTime+time) then ii=1 end hh=(hh*ii) end
 				self:setHeight(hh)
 				h=self:getHeight()
-				--i=math.min(1,ttt)
-				--i=ttt
-				local tw3=math.max(tw,tw2)
-				local ww=0
-				pcall(function() ww=self:getParent():getWidth()-p2-p4-10 end)
-				ww=tonumber(ww) or 0
-				tw=math.min(ww,tw3)
-				local ss=math.min(1,(ww/tw3))
-				pcall(function() self:setWidth((tw+10)) end)
-				w=self:getWidth()
-				pcall(function() self:setX(xx-((w+p4)*iii)) end)
-				--y=p3-(31*(1-ii))+hh-30
-				--y=math.max(p3,y+hh-30)
 				Drawing.setClipping(x,y,w,h)
 				Drawing.setAlpha(0.7)
+				Drawing.setColor(giGetColor())
 				Drawing.drawRect(x,y,w,h)
 				--pcall(function() Drawing.drawNinePatch(Draft.getDraft("$00000gi"):getFrame(1),x,y,w,h) end)
 				Drawing.setAlpha(0.5)
 				Drawing.setColor(giAutoGetColor())
 				drawOutline(x,y,w,h)
 				--pcall(function() Drawing.drawNinePatch(Draft.getDraft("$00000gi"):getFrame(10),x,y,w,h) end)
+				local xx=x
+				pcall(function() xx=self:getParent():getWidth() end)
+				xx=tonumber(xx) or x
+				local title=tostring(tbl.title or "")
 				Drawing.setAlpha(1)
 				Drawing.setScale(s,s)
-				Drawing.setColor(128,128,128)
-				Drawing.drawText(title,x+((tw+10)/2)-(tw/2),y,Font.BIG)
-				Drawing.setScale(s*ss,s*ss)
 				Drawing.setColor(giAutoGetColor())
-				th=th*ss
-				Drawing.drawText(text,x+((tw+10)/2)-(tw/2),y+20-(th/2),Font.BIG)
-				Drawing.setScale(1,1)
+				Drawing.setAlpha(0.5)
+				local tw=Drawing.getTextSize(title,Font.BIG)
+				self:setWidth(math.max(self:getWidth(),tw+10+ww()))
+				w=self:getWidth()
+				Drawing.drawText(title,x+5,y,Font.BIG)
+				Drawing.setAlpha(1)
+				local function drawText(text,x,y)
+					local text=tostring(text or "")
+					local tw,th=Drawing.getTextSize(text,Font.BIG)
+					local tw2=Drawing.getTextSize(title,Font.BIG)
+					tw,th,tw2=tw*s,th*s,tw2*s
+					Drawing.setColor(giGetColor())
+					--i=math.min(1,ttt)
+					--i=ttt
+					local tw3=math.max(tw,tw2)
+					local ww=0
+					pcall(function() ww=self:getParent():getWidth()-p2-p4-10 end)
+					ww=tonumber(ww) or 0
+					tw=math.min(ww,tw3)
+					local ss=math.min(1,(ww/tw3))
+					pcall(function() self:setWidth(math.max(self:getWidth(),(tw+10))) end)
+					w=self:getWidth()
+					pcall(function() self:setX(xx-((w+p4)*iii)) end)
+					Drawing.setScale(s*ss,s*ss)
+					Drawing.setColor(giAutoGetColor())
+					th=th*ss
+					Drawing.drawText(text,x+((tw+10)/2)-(tw/2),y-(th/2),Font.BIG)
+					Drawing.setScale(1,1)
+				end
+				local texts=tbl.text:split("\n")
+				while #texts>70 do table.remove(texts) end
+				thh=gth*#texts
+				for i,text in ipairs(texts) do drawText(text,x,y+6+(gth*i)) end
 				Drawing.resetClipping()
 			end
 		}
@@ -116,8 +181,8 @@ local addToast2=function(tbl)
 end
 local addToast=function(title,...)
 	local texts={}
-	for i,v in ipairs(table.pack(...)) do texts[i]=tostring(v or "") end
-	local tbl={h=30,title=tostring(title or ""),text=table.concat(texts,"\n"),tt=Runtime.getTime(),att=Runtime.getTime()}
+	for i,v in ipairs{...} do texts[i]=tostring(v or type(v)=="nil" and "") end
+	local tbl={h=30,startTime=300,endTime=300,time=8000,title=tostring(title or ""),text=table.concat(texts,"\n"),tt=Runtime.getTime(),att=Runtime.getTime()}
 	tbl.id=Runtime.getUuid()
 	local e=pcall(function()
 		local l=#toasts
@@ -126,9 +191,11 @@ local addToast=function(title,...)
 		local ttt=Runtime.getTime()-tt
 		if ttt<300 then
 			if #toasts>=1 then
-				toasts[l].text=toasts[l].text.."\n" ..table.concat(texts,"\n")
-				toasts[l].tt=Runtime.getTime()-300
-				toasts[l].att=Runtime.getTime()
+				if toasts[l].title==tbl.title then
+					toasts[l].text=table.concat(texts,"\n").."\n" ..toasts[l].text
+					if Runtime.getTime()-toasts[l].tt>300 then toasts[l].tt=Runtime.getTime()-300 end
+					toasts[l].att=Runtime.getTime()
+				else table.insert(toasts,tbl) end
 			else table.insert(toasts,tbl) end
 		else table.insert(toasts,tbl) end
 	end)
@@ -147,45 +214,95 @@ end
 Runtime.setClipboard=setClipboard
 local toast=function(...) addToast("Toast",...) end
 function script:buildCityGUI()
-	Debug.toast(Runtime.getStageName())
+	--Debug.toast(Runtime.getStageName())
 end
-function script:lateInit() Debug.toast=toast end
+local function mdd()
+	pcall(function() for _,v in pairs(Draft.getDrafts()) do pcall(function()
+	end) end end)
+	--Debug.toast(pcall(function() return tostring(Draft.getDraft("$prison00").orig.upgrades) end))
+end
 function script:enterStage(s)
 	pcall(function() GUI.get("toasts"):delete() end)
 	local tt=Runtime.getTime()
-	GUI.getRoot():addCanvas {
-		id="toasts",
-		onUpdate=function(self)
-			self:setSize(GUI.getRoot():getSize())
-			self:setSize(self:getWidth(),self:getHeight())
-			self:setPosition(-p2,-p3)
-			local ttt=(Runtime.getTime()-tt)/1000
-			if ttt>=5 then self:setChildIndex(self:getParent():countChildren()+1) tt=Runtime.getTime() end
-			local h=0
-			for i=1,self:countChildren() do
-				local c=self:getChild(self:countChildren()-i+1)
-				h=h+c:getHeight()+1
-				c:setY(h-c:getHeight()-1)
-			end
-		end,
-		onInit=function(self)
-			for _,tbl in pairs(toasts) do addToast2(tbl) end
-		end,
-	}:setTouchThrough(true)
-	Debug.toast=toast
+	if type(toast0)~="function" then toast0=Debug.toast end
+	if isToastRevampEnabled() then
+		GUI.getRoot():addCanvas {
+			id="toasts",
+			onUpdate=function(self)
+				self:setSize(GUI.getRoot():getSize())
+				self:setPosition(-p2,-p3)
+				local c=GUI.get("roottoolbar")
+				if c then if c:isVisible() and c:getChild(1) then self:setY(self:getY()+c:getChild(1):getHeight()) end end
+				self:setSize(self:getWidth(),self:getHeight()-self:getY())
+				local ttt=(Runtime.getTime()-tt)/1000
+				if ttt>=5 then self:setChildIndex(self:getParent():countChildren()+1) tt=Runtime.getTime() end
+				local h=0
+				for i=1,self:countChildren() do
+					local c=self:getChild(self:countChildren()-i+1)
+					h=h+c:getHeight()+1
+					c:setY(p3+(h-c:getHeight()-1))
+				end
+			end,
+			onInit=function(self)
+				for _,tbl in pairs(toasts) do addToast2(tbl) end
+				Runtime.postpone(function() self:setChildIndex(self:getParent():countChildren()+1) end)
+			end,
+		}:setTouchThrough(true)
+		Debug.toast=toast
+	else Debug.toast=toast0 end
 	--Debug.toast(s)
 end
-local fps,ofps,tt=0,0,Runtime.getTime()
-local function mdd()
-	pcall(function() for _,v in pairs(Draft.getDrafts()) do pcall(function()
-		
-	end) end end)
-end
 mdd()
+local function generateId()
+	local s=("abdeof"):rep(2).."eeaad"
+	--local s="qwertyuiopasdfghjklzxcvbnm"
+	s=s.."0123456789"
+	local tbl,tbl2={},{}
+	for v in s:gmatch(".") do
+		tbl[#tbl+1]=v
+	end
+	for _=1,8 do
+		local v,i=tbl[math.random(1,#tbl)],math.random(0,1)
+		if i==1 then v=v:upper() end
+		tbl2[#tbl2+1]=v
+	end
+	tbl2[#tbl2+1]="-"
+	for _=1,4 do
+		local v,i=tbl[math.random(1,#tbl)],math.random(0,1)
+		if i==1 then v=v:upper() end
+		tbl2[#tbl2+1]=v
+	end
+	tbl2[#tbl2+1]="-"
+	for _=1,4 do
+		local v,i=tbl[math.random(1,#tbl)],math.random(0,1)
+		if i==1 then v=v:upper() end
+		tbl2[#tbl2+1]=v
+	end
+	tbl2[#tbl2+1]="-"
+	for _=1,4 do
+		local v,i=tbl[math.random(1,#tbl)],math.random(0,1)
+		if i==1 then v=v:upper() end
+		tbl2[#tbl2+1]=v
+	end
+	tbl2[#tbl2+1]="-"
+	for _=1,10 do
+		local v,i=tbl[math.random(1,#tbl)],math.random(0,1)
+		if i==1 then v=v:upper() end
+		tbl2[#tbl2+1]=v
+	end
+	return table.concat(tbl2)
+end
+local tt,ott=Runtime.getTime()
+local fps,f=0
 function script:overlay()
 	pcall(function() while #toasts>30 do table.remove(toasts) end end)
 	pcall(function() for i,v in ipairs(toasts) do
-		if (Runtime.getTime()-v.tt>5000) or (tostring(v.text or "")=="") then table.remove(toasts,tonumber(i)) end
+		local time=tonumber(v.time) or 1000
+		local startTime=tonumber(v.startTime) or 1000
+		local endTime=tonumber(v.endTime) or 1000
+		if not TheoTown.SETTINGS.uiAnimations then startTime,endTime=0,0 end
+		local ii=startTime+endTime+endTime
+		if (Runtime.getTime()-v.tt>ii+time) then table.remove(toasts,tonumber(i)) end
 	end end)
 	pcall(function() p2,p3,p4,p5=GUI.getRoot():getPadding() end)
 	local ttt=(Runtime.getTime()%20000)/20000
@@ -200,11 +317,21 @@ function script:overlay()
 			if ii==2 then return NinePatch[k] end
 		end
 	end
-	fps=fps+1
-	local ttt=Runtime.getTime()-tt
-	if ttt>1000 then tt,ofps,fps=Runtime.getTime(),fps,0 end
-	--Drawing.drawText("FPS: "..ofps,0,0,Font.BIG)
-	mdd()
+	if giShowFPSCounter then
+		ott=Runtime.getTime()
+		fps=0
+		local i=ott-tt
+		fps=math.floor(1000/i)
+		tt=Runtime.getTime()
+		Drawing.setColor(255,255,255)
+		local text="FPS: "..fps
+		local tw,th=Drawing.getTextSize(text)
+		Drawing.setColor(0,0,0) Drawing.setAlpha(0.7)
+		local x,y=p2+5,p3+5
+		Drawing.drawRect(x,y,tw,th)
+		Drawing.setColor(255,255,255) Drawing.setAlpha(1)
+		Drawing.drawTextOutline(text,x,y)
+	end
 	pcall(function()
 		--for k,v in pairs(City) do
 			--if type(v)=="function" then City[k]=function() end end
@@ -213,4 +340,33 @@ function script:overlay()
 	end)
 	--Drawing.drawNinePatch(NinePatch.BUTTON,90,90,30,60*ttt)
 	--if type(e)=="function" then e() end
+	--error(Runtime.getUuid())
+	--Debug.toast(Runtime.getUuid())
+	--Debug.toast(generateId())
+	--Debug.toast(".")
+	f()
+end
+local tbl={}
+local s={{1,""}}
+local s2="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for v in s2:gmatch(".") do table.insert(tbl,v) end
+local i=1
+f=function()
+	local ss ss=function(i) if i<=#s then
+		local vv=s[i]
+		local v=tbl[vv[1]]
+		vv[2]=v
+		vv[1]=vv[1]+1
+		if vv[1]>#tbl then
+			vv[1]=1
+			if i>=#s then table.insert(s,{1,""}) end
+			ss(i+1)
+		end
+	end end 
+	ss(1)
+	local s3=""
+	for i,vv in ipairs(s) do
+		s3=vv[2]..s3
+	end
+	--Drawing.drawText(s3,100,100)
 end
